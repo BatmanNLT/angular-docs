d66876d5512e179ac1f1334986b3ece8f285b242
BatmanNLT <lekanetamba@icloud.com>
2022-05-17 17:49:04 +0200

    [feat](heroes) Created Mock heroes

---
+++ angular-tour-of-heroes/src/app/heroes/models/heroes.mock.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Deku' },
+    { id: 12, name: 'Dynamight' },
+    { id: 13, name: 'Shoto' },
+    { id: 14, name: 'All-might' },
+    { id: 15, name: 'Endeavor' },
+    { id: 16, name: 'Thunderbolt' },
+    { id: 17, name: 'Astaroth' },
+    { id: 12, name: 'Uravity' },
+    { id: 18, name: 'Creati' },
+    { id: 19, name: 'Midnight' },
+    { id: 20, name: 'Eraser-Head' },
+];
