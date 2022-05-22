e2269d0c519ded0880e485085ebd40ff796aa5ee
BatmanNLT <lekanetamba@icloud.com>
2022-05-22 17:02:44 +0200

    [feat](hero-service): fetch hero by id

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -16,4 +16,10 @@ export class HeroService {
     this.msgService.add('HeroService: fetched heroes');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find(h => h.id === id)!;
+    this.msgService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
